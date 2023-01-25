1736c8774ad7181c4180556a50902e46e23782d1
Exilic <tobias.engberg@exilic.se>
2023-01-25 18:54:45 +0100

    Reinitialized Spring Boot and first part

---
+++ src/main/java/se/lexicon/workshopweek4/WorkshopWeek4Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshopweek4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopWeek4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopWeek4Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/workshopweek4/WorkshopWeek4ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshopweek4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopWeek4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
